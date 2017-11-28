284aa564bc2948b85b88f574945f821a36fc9c9b
Alton Barbehenn <barbehenna@kenyon.edu>
2017-11-28 15:08:51 -0500

    Added course study script to verify parameter space and build analysis inferestructure

---
+++ CourseStudies.R
@@ -0,0 +1,6 @@
+# Copyright (c) 2017 Alton Barbehenn
+
+# This script loads a course, set of simulation parameters and their resulting statistics.
+# It then runs some of the interesting analyses to determine whether or not the ranges are 
+# good and if we want to make the resolution finer for more data points. 
+
