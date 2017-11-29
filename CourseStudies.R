fac8ffbbbe4f2324da2d95aecaaebd21bcb06ce1
Alton Barbehenn <barbehenna@kenyon.edu>
2017-11-29 18:51:56 -0500

    Cleaned up header

--- CourseStudies.R
+++ CourseStudies.R
@@ -6,26 +6,6 @@
 # for more data points. 
 
 library(data.table)
-
-Sim <- read.csv("data/StudyAggregate_2017-11-28_18-44-35.csv", header = TRUE, row.names = NULL)
-
-
-AggStats <- aggregate(cbind(dHat, nHat, pHat, pHatZeroNeg, pHatDropNeg, aHat) ~ square + TrapSpacing + FieldSize + CatchRadius + density, data = Sim, median)
-
-
-
-
-
-
-
-# Copyright (c) 2017 Alton Barbehenn
-
-# This script loads a course, set of simulation parameters and their resulting statistics 
-# (From MultiParameterSimulation.R). It then runs some of the interesting analyses to 
-# determine whether or not the ranges are good and if we want to make the resolution finer 
-# for more data points. 
-
-library(data.table)
 library(ggplot2)
 
 Sim <- read.csv("data/StudyAggregate_2017-11-28_18-44-35.csv", header = TRUE, row.names = NULL)
