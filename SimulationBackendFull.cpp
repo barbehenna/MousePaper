8423472856b436cdeb61a173cebd7df8e4d65f0d
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-01-24 09:11:14 -0600

    Added function to generate a single mouse and return it's foraging locations

--- SimulationBackendFull.cpp
+++ SimulationBackendFull.cpp
@@ -2,6 +2,22 @@
 using namespace Rcpp;
 
 
+// Generates a single mouse in a field of spcified size and returns all of the
+// locations the mouse forages (number of forages given by nforages).
+// In the returned matrix, each row is a forage coordinate (x,y).
+// [[Rcpp::export]]
+NumericMatrix GenMouse(int nforages, double fieldSize) {
+  NumericMatrix mouse(nforages, 2); // forage locations for a single mouse
+  
+  NumericVector home = runif(2); // x and y home coordinates
+  home = (home - 0.5) * fieldSize;
+  
+  mouse(_,0) = rnorm(nforages) + home[0]; // generate the forages
+  mouse(_,1) = rnorm(nforages) + home[1];
+  
+  return mouse;
+}
+
 
 // This function returns a matrix of trap coordinates, as such, the returned
 // matrix should be a nx2 matrix, (x,y) pairs
@@ -9,7 +25,7 @@ using namespace Rcpp;
 NumericMatrix GenerateTraps(int nrings = 8, double trapspacing = 1.0) {
   const int trapsperside = 2*nrings;
   double b[trapsperside];
-  NumericMatrix trapCoordinates(std::pow(trapsperside, 2), 2);
+  NumericMatrix trapCoordinates(trapsperside*trapsperside, 2);
   
   for (int i = 0; i < trapsperside; i++) {
     b[i] = (trapspacing/2) * ((2*i) - (trapsperside - 1));
