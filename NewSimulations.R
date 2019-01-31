046987b8fafd873ae32d9d364493e5732126fb14
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-01-31 09:04:31 -0600

    Reformated code to R standards

--- NewSimulations.R
+++ NewSimulations.R
@@ -1,3 +1,5 @@
+#### Libraries ####
+
 library(Rcpp)
 library(data.table)
 library(parallel)
@@ -17,7 +19,7 @@ dens.mode <- function(x, na.rm = FALSE) {
 
 #### Run Simulations ####
 
-# Define parameters for various simulations 
+# Define parameters for various simulations
 TrapSpacing <- seq(from = 0.25, to = 6, by = 0.25)
 CatchRadius <- seq(from = 0.25, to = 6, by = 0.25)
 Boarder <- seq(from = 3, to = 6, by = 6)
@@ -31,34 +33,33 @@ SimRuns <- rep(1:nrow(Parameters), each = 1000)
 SimRuns <- cbind(SimRuns, 1:length(SimRuns))
 
 # Run simulations
-# set.seed(0) # I think this works
-system.time(
-Simulations <- lapply(seq(nrow(SimRuns)), function(x) {
-  s <- SimRuns[x, ]
-  paramset <- Parameters[Parameters$paramset == s[1], ]
-  res <- RunSimulation(uuid = s[2], paramset = s[1], trapSpacing = paramset$TrapSpacing, catchRadius = paramset$CatchRadius, boarder = paramset$Boarder, nSquares = 8, trueDensity = paramset$Density, nForages = 4)
-  return(as.data.frame(res))
-})
-)
-Simulations <- rbindlist(Simulations)
-names(Simulations) <- c("uuid", "paramset", "square", "pd1", "pd2", "pHat", "nHat", "aHat", "dHat")
+# system.time(
+# Simulations <- lapply(seq(nrow(SimRuns)), function(x) {
+#   s <- SimRuns[x, ]
+#   paramset <- Parameters[Parameters$paramset == s[1], ]
+#   res <- RunSimulation(uuid = s[2], paramset = s[1], trapSpacing = paramset$TrapSpacing, catchRadius = paramset$CatchRadius, boarder = paramset$Boarder, nSquares = 8, trueDensity = paramset$Density, nForages = 4)
+#   return(as.data.frame(res))
+# })
+# )
+# Simulations <- rbindlist(Simulations)
+# names(Simulations) <- c("uuid", "paramset", "square", "pd1", "pd2", "pHat", "nHat", "aHat", "dHat")
 
 
 system.time(
-Simulations <- mclapply(seq(nrow(SimRuns)), function(x) {
-  s <- SimRuns[x, ]
-  paramset <- Parameters[Parameters$paramset == s[1], ]
-  res <- RunSimulation(uuid = s[2], paramset = s[1], trapSpacing = paramset$TrapSpacing, catchRadius = paramset$CatchRadius, boarder = paramset$Boarder, nSquares = 8, trueDensity = paramset$Density, nForages = 4)
-  return(as.data.frame(res))
-}, mc.cores = 4, mc.set.seed = TRUE)
+  Simulations <- mclapply(seq(nrow(SimRuns)), function(x) {
+    s <- SimRuns[x, ]
+    paramset <- Parameters[Parameters$paramset == s[1], ]
+    res <- RunSimulation(uuid = s[2], paramset = s[1], trapSpacing = paramset$TrapSpacing, catchRadius = paramset$CatchRadius, boarder = paramset$Boarder, nSquares = 8, trueDensity = paramset$Density, nForages = 4)
+    return(as.data.frame(res))
+  }, mc.cores = 2, mc.set.seed = TRUE)
 )
 Simulations <- rbindlist(Simulations)
 names(Simulations) <- c("uuid", "paramset", "square", "pd1", "pd2", "pHat", "nHat", "aHat", "dHat")
 
 
 # save results
-# fwrite(Parameters, "data/ParametersNewSample3.csv")
-# fwrite(Simulations, "data/SimulationsNewSample3.csv")
+fwrite(Parameters, "data/ParametersNewSample3.csv")
+fwrite(Simulations, "data/SimulationsNewSample3.csv")
 
 
 
@@ -77,69 +78,69 @@ Simulations <- fread("data/NewBackend-Simulations1.csv")
 Simulations <- merge(x = Simulations, y = Parameters, by = "paramset")
 
 # Computer true number of mice
-Simulations[, `:=`(nSquares =  max(square)),  by = .(uuid)] #number of squares used in specific simulation run
+Simulations[, `:=`(nSquares = max(square)), by = .(uuid)] # number of squares used in specific simulation run
 Simulations[, `:=`(nMice = round(Density * (2 * TrapSpacing * nSquares + 2 * Boarder)^2))] # re-calculate number of mice used
 
 
 
 # define a hopefully well-behaved subset of the data to explore
 SimSubset <- na.omit(Simulations)
-SimSubset <-  SimSubset[square == 3, ]
-SimSubset <-  SimSubset[is.finite(nHat), ]
-SimSubset <-  SimSubset[nHat > 0, ]
+SimSubset <- SimSubset[square == 3, ]
+SimSubset <- SimSubset[is.finite(nHat), ]
+SimSubset <- SimSubset[nHat > 0, ]
 
 
 
 # How good are we doing with our density estimates?
-ggplot(SimSubset) + geom_density(aes(x = dHat, colour = factor(Density))) + xlim(0,8)
+ggplot(SimSubset) + geom_density(aes(x = dHat, colour = factor(Density))) + xlim(0, 8)
 
 # Damn that's pretty, but how does it look when we standardize our estimates?
 # dHat/Density = 1 if we are accurate
-ggplot(SimSubset) + geom_density(aes(x = dHat/Density, colour = factor(Density))) + xlim(0,2)
+ggplot(SimSubset) + geom_density(aes(x = dHat / Density, colour = factor(Density))) + xlim(0, 2)
 
 # So it looks like regardless of how many mice we have (all things considered other than density)
 # our measurements are very good (the tails on our standardized densities aren't symmetric)
 
 # Why are the tails not symmetric? It could just be becuase they're bounded on the left
-ggplot(SimSubset) + geom_density(aes(x = dHat/Density, colour = factor(CatchRadius))) + xlim(0,2)
+ggplot(SimSubset) + geom_density(aes(x = dHat / Density, colour = factor(CatchRadius))) + xlim(0, 2)
 # So small CatchRadius is why we see heavy tails on the left, but they almost over correct for that issue/observation
-ggplot(SimSubset) + geom_density(aes(x = dHat/Density, colour = factor(TrapSpacing))) + xlim(0,2)
+ggplot(SimSubset) + geom_density(aes(x = dHat / Density, colour = factor(TrapSpacing))) + xlim(0, 2)
 # And low TrapSpacing is why we see heavy tails on the right
 
 # Look just at what should be a "good" combination
-ggplot(SimSubset[TrapSpacing==5 & CatchRadius==4]) + geom_density(aes(x = dHat/Density)) 
+ggplot(SimSubset[TrapSpacing == 5 & CatchRadius == 4]) + geom_density(aes(x = dHat / Density))
 # yeah... that looks pretty nice :)
 
 
 # Ok, let's parse out this more in 3d
-dHatSummary <- SimSubset[, .(dHat.mean = mean(dHat/Density), dHat.var = var(dHat/Density)), by = .(TrapSpacing, CatchRadius)]
+dHatSummary <- SimSubset[, .(dHat.mean = mean(dHat / Density), dHat.var = var(dHat / Density)), by = .(TrapSpacing, CatchRadius)]
 
 # Using log10 dHat.mean for to center the colors around the correct value (dHat/Density = 1 => log(dHat/Density) = 0)
-p <- dHatSummary %>% 
-  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~log10(dHat.mean)) %>%
+p <- dHatSummary %>%
+  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~ log10(dHat.mean)) %>%
   add_markers()
 p
 
 # We have variance information, let's plot that too with size
-p <- dHatSummary %>% 
-  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~log10(dHat.mean), size = ~dHat.var, marker = list(symbol = 'circle', sizemode = 'diameter')) %>%
+p <- dHatSummary %>%
+  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~ log10(dHat.mean), size = ~dHat.var, marker = list(symbol = "circle", sizemode = "diameter")) %>%
   add_markers()
 p
 
 # Looks good, but let's crop it a little so that it's easier to read
-p <- dHatSummary[TrapSpacing>0.25 & CatchRadius>0.25] %>% 
-  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~log10(dHat.mean), size = ~dHat.var, marker = list(symbol = 'circle', sizemode = 'diameter')) %>%
+p <- dHatSummary[TrapSpacing > 0.25 & CatchRadius > 0.25] %>%
+  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~ log10(dHat.mean), size = ~dHat.var, marker = list(symbol = "circle", sizemode = "diameter")) %>%
   add_markers()
 p
 
 # Just for fun, look at only those points whose mean dHat/Density value is within 5% of correct
-p <- dHatSummary[abs(dHat.mean-1)<0.05] %>% 
-  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~log10(dHat.mean), size = ~dHat.var, marker = list(symbol = 'circle', sizemode = 'diameter')) %>%
+p <- dHatSummary[abs(dHat.mean - 1) < 0.05] %>%
+  plot_ly(x = ~TrapSpacing, y = ~CatchRadius, z = ~dHat.mean, color = ~ log10(dHat.mean), size = ~dHat.var, marker = list(symbol = "circle", sizemode = "diameter")) %>%
   add_markers()
 p
 
 
-# save any of these plots as interactive html's using 
+# save any of these plots as interactive html's using
 # htmlwidgets::saveWidget(widget = as_widget(p), file = "~/Desktop/3d-dHat_plot.html")
 
 
@@ -152,18 +153,14 @@ p
 # There is a true catch radius, how can we find that or how do we run an expirament around that?
 
 # Repeat at a few differnt trap spacings (along one of the lines below)
-ggplot(dHatSummary[is.element(CatchRadius, c(1,2,3,4,5))]) + 
+ggplot(dHatSummary[is.element(CatchRadius, c(1, 2, 3, 4, 5))]) +
   geom_hline(yintercept = 0) +
-  geom_path(aes(x = TrapSpacing, y = log(dHat.mean), colour = factor(CatchRadius), linetype = factor(CatchRadius))) + 
+  geom_path(aes(x = TrapSpacing, y = log(dHat.mean), colour = factor(CatchRadius), linetype = factor(CatchRadius))) +
   ggtitle("Samples of Cross-sections From 3d Plot", subtitle = "Goal is dHat.mean = 1, drawn in black")
 
 # It looks like we expect: for each curve there's a region where it performs quite well and in the middle,
-# but, in general, it looks something like a downward cubic function. To the left of the good region, the 
+# but, in general, it looks something like a downward cubic function. To the left of the good region, the
 # estimator is consistently way too high and to the right it is too low. Looking at how it changes by Catch Radius
 # we see that the good region shifts to the right as Catch Radius increases. This makes sense because we expect
-# the method to give the best results when CT ~= TS/2. A much larger set of Catch Radius and Trap Spacing 
+# the method to give the best results when CT ~= TS/2. A much larger set of Catch Radius and Trap Spacing
 # pairs may be needed to define exactly where the method works best (by eye I'd guess 2CR <= TS <= 3CR)
-
-
-
-
