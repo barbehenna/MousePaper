aff1507a58a375bf61b31785bea88b10d08f49f6
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-02-03 22:35:41 -0600

    Added NA padding to backwards approximation and fixed indexes to be more natural for central approximation

--- DerivativeApprox.cpp
+++ DerivativeApprox.cpp
@@ -9,10 +9,10 @@ NumericVector SecondOrderCentral(NumericVector x, double h = 1) {
   NumericVector out(x.size());
   
   out[0] = NA_REAL;
-  for (int i = 2; i < x.size(); i++) {
-    out[i-1] = (x[i-2] - (2*x[i-1]) + x[i])/(h*h);
+  for (int i = 1; i < x.size()-1; i++) {
+    out[i] = (x[i-1] - (2*x[i]) + x[i+1])/(h*h);
   }
-  out[out.size()-1] = NA_REAL;
+  out[x.size()-1] = NA_REAL;
   
   return out;
 }
@@ -24,13 +24,11 @@ NumericVector SecondOrderCentral(NumericVector x, double h = 1) {
 NumericVector SecondOrderForward(NumericVector x, double h = 1) {
   NumericVector out(x.size());
   
-  printf("size:  %lu\n", x.size());
   for (int i = 0; i < x.size()-2; i++) {
-    printf("i=%d: %f %f %f\n", i, x[i],  x[i+1], x[i+2]);
     out[i] = (x[i+2] - 2*x[i+1] + x[i])/(h*h);
   }
-  out[out.size()-2] = NA_REAL;
-  out[out.size()-1] = NA_REAL;
+  out[x.size()-2] = NA_REAL;
+  out[x.size()-1] = NA_REAL;
   
   return out;
 }
@@ -40,10 +38,12 @@ NumericVector SecondOrderForward(NumericVector x, double h = 1) {
 // assuming constant step size
 // [[Rcpp::export]]
 NumericVector SecondOrderBackward(NumericVector x, double h = 1) {
-  NumericVector out(x.size()-2);
+  NumericVector out(x.size());
   
+  out[0] = NA_REAL;
+  out[1] = NA_REAL;
   for (int i = 2; i < x.size(); i++) {
-    out[i-1] = (x[i] - 2*x[i-1] + x[i-2])/(h*h);
+    out[i] = (x[i] - 2*x[i-1] + x[i-2])/(h*h);
   }
   
   return out;
