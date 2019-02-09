9c0a6f0b5c3df3b9bbd11122f988eeab4fa8e4f6
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-02-09 14:31:05 -0600

    more analysis on accuracy by square

--- NewSimulations.R
+++ NewSimulations.R
@@ -86,8 +86,7 @@ Simulations[, `:=`(nMice = round(Density * (2 * TrapSpacing * nSquares + 2 * Boa
 square.results <- Simulations[!is.na(dHat) & is.finite(dHat) & dHat > 0, .(mean.dHat = mean(dHat / Density, na.rm = TRUE), median.dHat = median(dHat / Density, na.rm = TRUE), mode.dHat = dens.mode(dHat / Density, na.rm = TRUE), var.dHat = var(dHat / Density, na.rm = TRUE)), by = .(square)]
 
 square.results <- square.results %>% 
-  gather(key = statistic, value = value, -square) %>%
-  arrange(square)
+  gather(key = statistic, value = value, -square)
 
 ggplot(square.results, aes(x = square, y = value, colour = statistic)) + geom_point(size = 2) + geom_line()
 
@@ -180,7 +179,12 @@ ggplot(dHatSummary[is.element(CatchRadius, c(1, 2, 3, 4, 5))]) +
 #### Experimenting ####
 
 Simulations <- Simulations[!is.na(dHat) & is.finite(dHat) & dHat>0, ]
-Temp.Sim <- Simulations[, .(median.dHat = median(dHat/Density)), by = .(square, TrapSpacing, CatchRadius)]
+Temp.Sim <- Simulations[, .(median.dHat = median(dHat/Density), var.dHat = var(dHat/Density)), by = .(square, TrapSpacing, CatchRadius)]
+
+
+ggplot(Temp.Sim, aes(x = square, y = median.dHat, colour = factor(CatchRadius), shape = factor(TrapSpacing))) + geom_point() + geom_line() + geom_hline(yintercept = 1)
+ggplot(Temp.Sim, aes(x = square, y = log(median.dHat), colour = factor(CatchRadius), shape = factor(TrapSpacing))) + geom_point() + geom_line() + geom_hline(yintercept = 0)
+# too messy
 
 ggplot(Temp.Sim, aes(x = square, y = median.dHat, colour = cut(log(CatchRadius/TrapSpacing), breaks = 6))) + geom_point() + geom_hline(yintercept = 1)
 # So it looks like if  log(CR/TS) is within (-1.06, 1.06) ie CR/TS is within 0.35 and 2.88 (or it's probably 1/e to e) then our model
@@ -192,3 +196,83 @@ ts = 4
 ggplot(Temp.Sim[TrapSpacing == ts, ], aes(x = square, y = median.dHat, colour = factor(CatchRadius))) + geom_point() + geom_line() + geom_hline(yintercept = 1) + ggtitle(paste("slice of plot by ts =", ts))
 # so in some cases there is an observable elbow, but it doesn't look like it happens at the same places for every cr
 # and it doesn't necessarily happen at all.
+
+
+
+tmp <- Temp.Sim[TrapSpacing==4 & CatchRadius==2.5]
+ggplot(tmp, aes(x = square, y = median.dHat)) + geom_point() + geom_smooth(method = "loess")
+ggplot(tmp, aes(x = square, y = var.dHat)) + geom_point() + geom_smooth(method = "loess")
+
+
+
+
+tmp <- tmp[sample(8)] # assume .DS is randomly ordered
+tmp <- tmp[order(square)]
+
+# method 1 of picking the square:
+# sort by square, look for a change in concavity. As square increases, approximate the second order derivative
+# and look to see if it changes. Becuase the second order derivative needs three points to define it, we can 
+# use left recursive approx, right recursive approx, or the centered method (maybe average them all?).
+# I'll  start with just the left sided method for ease of implementation.
+
+# Since we're only taking steps of size one, the running difference approximates the slope (f(x+h)-f(x))/h, h=1
+# Similarly, to approximate the second order deriavaticve, it suffices to look at the running difference of those
+# differences (f'(x+h)-f'(x))/h, h=1, f' approximated above.
+
+# We're essentially fitting a parabolla at every set of three consecutive points. Because we expect dHat to increase
+# at the edge, maybe it makes more sense to be conservative and only use the right parabolla. Of course, if we do so,
+# we will never select square 7 or 8. 
+
+tmp <- tmp[sample(8)] # assume .DS is randomly ordered
+tmp <- tmp[order(square)]
+
+first.order <- diff(tmp$median.dHat) #approx first order derivative at squares 1-7
+second.order <- diff(first.order) #approx second order derivative at squares 1-6
+# I THINK if I sort the squares 8,7,...,1, this method will give me the second order derivatives at squares 3-8
+
+
+Temp.Sim[, `:=`(crts = paste(CatchRadius, TrapSpacing, sep = "_"))]
+sims.list <- split(x = Temp.Sim, f = Temp.Sim$crts)
+sims.list <- lapply(sims.list, function(x) {
+  x <- x[order(square)]
+  second.order <- diff(diff(x[, median.dHat]))
+  return(min(c(which(second.order > 0.05)[1], 7), na.rm = TRUE))
+})
+sims.list <- stack(sims.list)
+names(sims.list) <- c("elbow.est","coord")
+
+Temp.Sim <- merge(x = Temp.Sim, y = sims.list, by.x = "crts", by.y = "coord")
+
+ggplot(Temp.Sim[TrapSpacing==3], aes(x = square, y = median.dHat, colour = factor(CatchRadius))) + geom_point() + geom_line()
+
+
+
+Simulations <- Simulations[uuid %in% sample(x = max(uuid), size = 20)]
+SimSplit <- split(Simulations, Simulations$uuid)
+
+tmp <- SimSplit[[2]]
+
+ggplot(tmp, aes(x = square, y = dHat)) + geom_point() + geom_line() 
+
+tmp <- tmp[order(-square)]
+sec.der <- rev(diff(diff(tmp[, dHat]))) # it was approx second order derivatives at squares 8,7,...,3, the rev makes it from 3,4,...,7,8
+which(abs(sec.der) < 0.05)[1]
+max(c(2, which(abs(sec.der) < 0.05)[1]), na.rm = TRUE)
+
+lapply(SimSplit, function(x) {
+  x <- x[order(-square)]
+  sec.der <- rev(diff(diff(x[, dHat])))
+  return(c("uuid" = x[1,uuid], "good.square" = max(c(2, which(abs(sec.der) < 0.05)[1]), na.rm = TRUE)))
+})
+
+
+
+
+
+
+
+
+
+
+
+
