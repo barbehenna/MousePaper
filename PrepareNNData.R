13b5a1523c0c2fe6425c3fa09bdf395bdefcb92f
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-06-04 22:08:43 -0500

    Added script to take simulation output to make a datatable that ignores any sigma related terms (for expiramenting)

---
+++ PrepareNNData.R
@@ -0,0 +1,34 @@
+#### Libraries ####
+
+library(data.table)
+library(tibble)
+library(dplyr)
+library(tidyr)
+
+
+#### Load Data ####
+
+Parameters <- fread("data/NewBackend-ParametersFull_B3.csv")
+Simulations <- fread("data/NewBackend-SimulationsFull_B3.csv")
+
+# merge in parameters
+Simulations <- merge(x = Simulations, y = Parameters, by = "paramset")
+
+# Remove invalid simulations
+# Approx 1.7% of simulations, also cleans up dHat
+Simulations <- Simulations[nHat >= 0]
+Simulations <- Simulations[is.finite(nHat)]
+Simulations <- Simulations[is.finite(pHat)]
+
+
+#### Prep Data ####
+
+Simulations %>%
+  mutate(square = paste0("square", square)) %>%
+  select(uuid, square, dHat, Density) -> Simulations
+
+Simulations = dcast(Simulations, ... ~ square, fill = NA, value.var = "dHat")
+
+head(Simulations)
+
+fwrite(Simulations, "data/trainingdata.csv")
