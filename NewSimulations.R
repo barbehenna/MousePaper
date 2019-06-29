7b6738a4ad0f31e9bbf18f81250efeb889ad3771
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-06-29 07:40:56 -0500

    Added random parameter simulations

--- NewSimulations.R
+++ NewSimulations.R
@@ -14,13 +14,22 @@ sourceCpp("cpp/SimulationBackendFull.cpp")
 TrapSpacing <- seq(from = 0.1, to = 4, by = .1)
 CatchRadius <- seq(from = 0.1, to = 4, by = 0.1)
 Boarder <- seq(from = 3, to = 6, by = 3)
-Density <- seq(from = 0.5, to = 5, by = 1)
+Density <- seq(from = 0.5, to = 5, by = 0.5)
 Parameters <- expand.grid(Density, Boarder, CatchRadius, TrapSpacing)
 names(Parameters) <- c("Density", "Boarder", "CatchRadius", "TrapSpacing")
 Parameters$paramset <- 1:nrow(Parameters)
 
+
+# Random paramset, not grided
+numsim = 4e+6
+Parameters <- data.frame(Density = runif(numsim, min=0.1,max=5), 
+                         TrapSpacing = runif(numsim, min=0.05, max=4),
+                         CatchRadius = runif(numsim, min=0.05, max=4),
+                         Boarder = rep(c(3,6), each=numsim/2),
+                         paramset = 1:numsim)
+
 # Make array specifying each simulation to be done
-SimRuns <- rep(1:nrow(Parameters), each = 1000)
+SimRuns <- rep(1:nrow(Parameters), each = 1)
 SimRuns <- cbind(SimRuns, 1:length(SimRuns))
 
 # Run simulations
@@ -49,9 +58,9 @@ names(Simulations) <- c("uuid", "paramset", "square", "pd1", "pd2", "pHat", "nHa
 
 
 # save results
-fwrite(Parameters, "data/ParametersNewSample.csv")
-fwrite(Simulations, "data/SimulationsNewSample.csv")
-
+fwrite(Parameters, "data/ParametersNewSample-random.csv")
+fwrite(Simulations, "data/SimulationsNewSample-random.csv")
+ 
 
 
 
